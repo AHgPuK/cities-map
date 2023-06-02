968bbb77eae9b13a53d77e57a720e91459d29afa
Andrey Hohutkin <andrey.hohutkin@gmail.com>
2023-06-02 13:44:16 +0300

    Update data

--- update.cmd
+++ update.cmd
@@ -1,11 +1,12 @@
 rem set FILE=allCountries.zip
 set FILE=cities1000
 
-rem Download
 curl http://download.geonames.org/export/dump/%FILE%.zip -o data/data.zip
 
 rem Extract
-7z e data/data.zip -odata %FILE%.txt
+7z e data/data.zip -y -odata %FILE%.txt
 
 rem Delete archive
 del /Q data\data.zip
+
+pause
